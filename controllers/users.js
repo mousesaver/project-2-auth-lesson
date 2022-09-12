490440ef46f947bbd0b358e8d372957b82c7a881
mousesaver <sean.fan87@gmail.com>
2022-09-12 13:58:00 -0700

    Part2 done

--- controllers/users.js
+++ controllers/users.js
@@ -19,13 +19,46 @@ router.post('', async (req, res) => {
         res.redirect('/')
     } catch(err) {
         console.log(err)
-    }
-    
-    
-    
-    
+    } 
 })
-
+// GET /users/login -- shows a login form to the user
+router.get('/login', (req, res) => {
+    res.render('users/login', {
+        message: req.query.message ? req.query.message : null
+    })
+})
+// POST / users/login -- accept a payload of form data and use it log a user in
+router.post('/login', async (req, res) => {
+    try {
+        // look uo the user in the db using the supplied email
+        const user = await db.user.findOne({
+            where : {
+                email: req.body.email
+            }
+        })
+        const message = "Incorrect username or password"
+        if (!user) {
+            console.log('message')
+            res.redirect('/users/login?message=' + message)
+        } else if (user.password !== req.body.password) {
+            console.log('message')
+            res.redirect('/users/login?message=' + message)
+        } else {
+            console.log('loggin the user in')
+            res.cookie('userId', user.id)
+            res.redirect('/')
+        }
+    } catch (err) {
+        console.log(err)
+        res.send('server eror')
+    }
+})
+// GET /users/logout -- log out a user by clearing the stored cookie
+router.get('/logout', (req, res) => {
+    // clear the cookie
+    res.clearCookie('userId')
+    res.redirect('/')
+})
 
 
 module.exports = router;
