1b44e66ddb057dc0000dc5a5afcfcfedc74468db
mousesaver <sean.fan87@gmail.com>
2022-09-12 14:18:49 -0700

    Part 3

--- index.js
+++ index.js
@@ -2,6 +2,8 @@
 require('dotenv').config()
 const express = require('express')
 const ejsLayouts = require('express-ejs-layouts')
+const cookieParser = require('cookie-parser')
+const db = require('./models')
 
 // config express app/middlewares
 const app = express()
@@ -9,8 +11,28 @@ const PORT = process.env.PORT || 3000
 app.set('view engine', 'ejs')
 app.use(ejsLayouts)
 app.use(express.urlencoded({extended: false}))
+app.use(cookieParser())
+// Our customer auth middleware
+app.use(async (req, res, next) => {
+    res.locals.myData = 'hello, fellow route'
+    // console.log('hello from a middleware')
+    // if there is a cookie on the incoming request
+    if (req.cookies.userId) {
+        const user = await db.user.findByPk(req.cookies.userId)
+        res.locals.user = user
+    } else {
+        res.locals.user = null
+    }
+    // move on to the next route or middleware in the chain
+    next()
+})
+
+
 // route definition
 app.get('/', (req, res) =>{
+    // console.log('incoming cookie 🍪', req.cookies)
+    // console.log(res.locals.myData)
+    console.log('The currently logged user is ', res.locals.user)
     res.render("home")
 })
 
